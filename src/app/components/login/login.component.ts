5347a5bc33d1012a297dfd52aecc49ad3476d259
DESKTOP\rober <robertopclase@gmail.com>
2023-11-10 20:04:51 +0100

    HTML structure and styles

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  title: string = 'Login';
+  username: string = 'Prueba técnica';
 
+  mobile: boolean = false;
+  rememberMe: boolean = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.mobile = window.innerWidth <= 960;
+  }
+
+  logIn() {
+    console.log('‘OK’');
+  }
+
+  changeRememberMe() {
+    this.rememberMe = !this.rememberMe;
+    console.log('Remember me: ', this.rememberMe);
+  }
 }
