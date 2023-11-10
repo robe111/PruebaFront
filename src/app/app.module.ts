3f77dbd09533c448defc22c4ca2fe4c291651e1f
DESKTOP\rober <robertopclase@gmail.com>
2023-11-10 21:37:25 +0100

    Form functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [],
-  imports: [FormsModule],
+  imports: [],
 })
 export class AppModule {}
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,19 @@
 import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   title: string = 'Login';
   username: string = 'Prueba técnica';
+  user: any;
 
   mobile: boolean = false;
   rememberMe: boolean = false;
@@ -26,10 +28,36 @@ export class LoginComponent {
     console.log('Remember me: ', this.rememberMe);
   }
 
-  logIn() {
-    console.log('‘OK’');
+  logIn(form: NgForm) {
+    this.validateParams(form);
   }
-  /*logIn(form: NgForm) {
-    console.log('‘OK’');
-  }*/
+
+  validateParams(form: NgForm) {
+    if (
+      this.validateEmail(form.value.email) &&
+      this.validatePass(form.value.password)
+    ) {
+      console.log('‘OK’');
+      console.log('Bienvenido');
+    } else {
+      // TODO: Mostrar mensaje de error
+      console.error('Error en el login');
+    }
+    if (form.value.rememberMe) {
+      console.log('Añadimos a sesionStorage');
+    }
+  }
+
+  validateEmail(email: string) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
+
+  validatePass(password: string) {
+    if (password.length < 5) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
